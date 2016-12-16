44359c0f8ad146586d309b963b6b2b5dfd9f7b04
moribaleta <moribaleta@gmail.com>
2016-12-16 08:50:52 +0800

    removed forecast_data pane on main.html

--- weather.js
+++ weather.js
@@ -40,7 +40,7 @@ function displayLocation(latitude,longitude){
                 data +"&#x2103;";*/
                 data = JSON.parse(data);
                 data_content.innerHTML = data[0]+"</br>"+data[1]+"</br>"+data[2];
-                data_content.innerHTML = "<br><a href='forecast.php'>view forecast</a>";
+                data_content.innerHTML += "<br><a href='forecast.php'>view forecast</a>";
                 document.getElementById('data_content').appendChild(data_content);
                 //document.getElementById('forecast_data').src = 'forecast.php';
             });
