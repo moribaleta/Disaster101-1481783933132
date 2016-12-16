22502de3b0c6fa9330c2cbf4a610586420afa951
moribaleta <moribaleta@gmail.com>
2016-12-16 09:45:55 +0800

    changed forecast to current

--- multipleforecast.php
+++ multipleforecast.php
@@ -16,9 +16,10 @@
 function getForecast($city){
 
     $key = 'af1d7670685d4b8389723009161212';
-    $forcast_days='7';
-    $url ="http://api.apixu.com/v1/forecast.json?key=$key&q=$city&days=$forcast_days";
-    $ch = curl_init();
+    //$forcast_days='7';
+    //$url ="http://api.apixu.com/v1/forecast.json?key=$key&q=$city&days=$forcast_days";
+    $url = "http://api.apixu.com/v1/current.json?key=$key&q=$city&=" ;
+    /*$ch = curl_init();
     curl_setopt($ch,CURLOPT_URL,$url);
     curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
     $json_output=curl_exec($ch);
@@ -33,13 +34,36 @@ function getForecast($city){
     $days = $weather->forecast->forecastday;
 
     foreach ($days as $day){
-        echo "<table>";
+        echo "<table><tr>$city</tr>";
         echo "<tr><td colspan='4' border='0'>condition: {$day->day->condition->text} <img src=' {$day->day->condition->icon}'/></td></tr>";
         echo "<tr><td>&nbsp;</td><td>Max.<br>Temprature</td><td>Min.<br>Temprature</td><td>Avg.<br>Temprature</td></tr>";
         echo "<tr><td>&deg;C</td><td>{$day->day->maxtemp_c}</td><td>{$day->day->avgtemp_c}</td></tr>";
         echo "<tr><td><h4>Wind</h4></td><td colspan='3'>{$day->day->maxwind_kph}kph </td></tr>";
         echo "</table> <br>";
-    }
+    }*/
+    $ch = curl_init();
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+
+    $json_output=curl_exec($ch);
+    $weather = json_decode($json_output);
+    echo "<h1>Current Weather</h1>";
+    echo "<h2>Location</h2>";
+    echo "City: ". $weather->location->name;
+    echo "<br>";
+    echo "<h2>Temprature</h2>";
+    echo "<br>";
+    echo "Temperature (&deg;C): " . $weather->current->temp_c; echo "<br>";
+    echo "Feels like (&deg;C)". $weather->current->feelslike_c;
+    echo "<br>";
+    echo "Condition: <img src='" . $weather->current->condition->icon ."'>" . $weather->current->condition->text;
+    echo "<h2>Wind</h2>";
+    echo $weather->current->wind_kph." kph <br>";
+    echo $weather->current->wind_degree."&deg;  " . $weather->current->wind_dir."<br>";
+    echo "Humidity: ".$weather->current->humidity;
+    echo "<br><br><br>";
+
+    echo "Updated On: ".$weather->current->last_updated;
 
 }
 
