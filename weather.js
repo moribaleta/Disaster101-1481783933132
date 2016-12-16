072bb52a799bfb4eee8427088f782b2c0dab4c38
josiah-awaao <josiahawa.ao@gmail.com>
2016-12-17 00:13:02 +0800

    dump files deleted

--- weather.js
+++ weather.js
@@ -38,10 +38,6 @@ function displayLocation(latitude,longitude){
                 console.log(data);
                 var data_content = document.createElement("h2");
 
-                var hello = "hello";
-
-                window.location.href = "https://disaster101.mybluemix.net/testing.php?w1=" + hello;
-
                  /*data = data.substr(1,data.length-2);
                 data +"&#x2103;";*/
                 data = JSON.parse(data);
