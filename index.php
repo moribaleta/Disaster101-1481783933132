2996fbeb7bbcfd73611561036b6b59cdf15990b2
moribaleta <moribaleta@gmail.com>
2016-12-16 13:40:11 +0800

    edited icon

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>PHP Starter Application</title>
+        <title>DISASTER101</title>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <link rel="stylesheet" href="style.css" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
