99ddfef4996b974e4bd44fdc98e87dbade05e639
moribaleta <moribaleta@gmail.com>
2016-12-16 13:50:59 +0800

    edited forecast

--- forecast.php
+++ forecast.php
@@ -26,18 +26,20 @@
 
 
         $days = $weather->forecast->forecastday;
+        echo "<table><tr>$city</tr>";
 
         foreach ($days as $day){
 
-            echo "<table>";
+
             echo "<tr><td colspan='4' border='0'><h2>{$day->date}</h2> Sunrise: {$day->astro->sunrise} <br> Sunset: {$day->astro->sunset}"
                 . "<br> condition: {$day->day->condition->text} <img src=' {$day->day->condition->icon}'/></td></tr>";
             echo "<tr><td>&nbsp;</td><td>Max.<br>Temprature</td><td>Min.<br>Temprature</td><td>Avg.<br>Temprature</td></tr>";
 
-            echo "<tr><td>&deg;C</td><td>{$day->day->maxtemp_c}</td><td>{$day->day->mintemp_f}</td><td>{$day->day->avgtemp_c}</td></tr>";
-            echo "<tr><td><h4>Wind</h4></td><td colspan='3'>{$day->day->maxwind_mph}Mph <br> {$day->day->maxwind_kph}kph </td></tr>";
-            echo "</table> <br>";
+            echo "<tr><td>&deg;C</td><td>{$day->day->maxtemp_c}</td><td>{$day->day->avgtemp_c}</td></tr>";
+            echo "<tr><td><h4>Wind</h4></td><td colspan='3'>{$day->day->maxwind_kph}kph </td></tr>";
+
         }
+        echo "</table> <br>";
         ?>
     </body>
 </html>
