627dd797b94f3ba083ffdcdae5e6bc347d391b13
moribaleta <moribaleta@gmail.com>
2016-12-16 22:05:48 +0800

    added header  on tone

--- toneanalyzer.php
+++ toneanalyzer.php
@@ -4,12 +4,14 @@
 */
 $username = "aae7697b-3ffa-4b5f-9cd9-24ea65720e4e";
 $password = "E03IT2NglRD5";
+$header = ["Content-Type: application/json"];
 $params = ['version'=>'2016-05-19','text'=>'A%20word%20is%20dead%20when%20it%20is%20said,%20some%20say.%20Emily%20Dickinson'];
 $defaults = array(
     CURLOPT_URL => 'https://gateway.watsonplatform.net/tone-analyzer/api/v3/tone',
     CURLOPT_USERPWD => "$username:$password",
     CURLOPT_POST => true,
     CURLOPT_POSTFIELDS => $params,
+    CURLOPT_HTTPHEADER => $header
 );
 $ch = curl_init();
 curl_setopt_array($ch,$defaults);
