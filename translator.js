f311c8f6ac97225353a333c85e0feb1262a98752
moribaleta <moribaleta@gmail.com>
2016-12-17 05:21:25 +0800

    edited js file

--- translator.js
+++ translator.js
@@ -1,17 +1,18 @@
 function translateData(text,id){
     var data_return = text;
-    /*$.post('translator.php',{text: text},function(data){
+    $.post('translator.php',{text: text},function(data){
         console.log(data);
-        /*data = data.substr(1,data.length-2);
-                data +"&#x2103;";
-        //data = JSON.parse(data);
-        data_return = data;
+
+        data = JSON.parse(data);
+        console.log(data);
+        document.getElementById(id).textContent = JSON.parse(data);
         /*data_content.innerHTML = data[0]+"</br>"+data[1]+"</br>"+data[2];
         data_content.innerHTML += "<br><a href='forecast.php'>view forecast</a>";
         document.getElementById('data_content').appendChild(data_content);
-        //document.getElementById('forecast_data').src = 'forecast.php';
-    });*/
-    $.ajax({
+        //document.getElementById('forecast_data').src = 'forecast.php';*/
+    });
+
+    /*$.ajax({
         type: "POST",
         async:false, // ** CHANGED **
         url: "translator.php",
@@ -23,7 +24,7 @@ function translateData(text,id){
         , error: function () {
             chyba("chyba v požadavku", "df");
         }
-    });
+    });*/
     console.log(data_return);
     return data_return;
 }
