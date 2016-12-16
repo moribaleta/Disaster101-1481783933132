e907330660a30f6ed7a2f1dca90c7a511ab2682a
moribaleta <moribaleta@gmail.com>
2016-12-17 04:29:29 +0800

    return statement

--- translator.js
+++ translator.js
@@ -1,13 +1,15 @@
 function translateData(text){
+    var data_return;
     $.post('translator.php',{text: text},function(data){
         console.log(data);
         /*data = data.substr(1,data.length-2);
                 data +"&#x2103;";*/
         //data = JSON.parse(data);
-        return data;
+        data_return = data;
         /*data_content.innerHTML = data[0]+"</br>"+data[1]+"</br>"+data[2];
         data_content.innerHTML += "<br><a href='forecast.php'>view forecast</a>";
         document.getElementById('data_content').appendChild(data_content);*/
         //document.getElementById('forecast_data').src = 'forecast.php';
     });
+    return data_return;
 }
