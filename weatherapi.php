f4aab67f534ea83ef3eeb07ac6944890f4d58cbc
moribaleta <moribaleta@gmail.com>
2016-12-16 08:25:44 +0800

    added current condition

--- weatherapi.php
+++ weatherapi.php
@@ -10,13 +10,13 @@ $city = $geo["geoplugin_city"];
 */
 $key = 'af1d7670685d4b8389723009161212';
 
-$url = "https://api.apixu.com/v1/current.json?key=$key&q=$city&=";
+$url = "https://api.apixu.com/v1/current.json?key=$key&q=$city";
 $ch = curl_init();  
 curl_setopt($ch,CURLOPT_URL,$url);
 curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 $json_output=curl_exec($ch);
 $weather = json_decode($json_output);
 
-$return = array("City: ".$city,"Temperature: ". $weather->current->temp_c);
+$return = array("City: ".$city,"Temperature: ". $weather->current->temp_c ."Condition:" $weather->current->condition->text);
 echo json_encode($return);
 ?>
