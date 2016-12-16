a400a997ab3f0630548a4b085d259167c6a95c96
josiah-awaao <josiahawa.ao@gmail.com>
2016-12-16 23:57:53 +0800

    nah

--- testing.php
+++ testing.php
@@ -8,5 +8,8 @@ if(isset ($_GET["w1"])) {
     echo "<script language = 'text/javascript'>function sayHiFromPHP() { alert('Just wanted to say $hello!'); }</script>";
 }
 
+else
+    echo "wtf";
+
 
 ?>
